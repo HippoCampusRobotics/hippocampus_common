03417d7b4533d73cc5c269e6f3f2f8e716689ebf
Thies Lennart Alff <thies.lennart.alff@tuhh.de>
2020-04-28 13:58:09 +0200

    added position transformation

--- src/hippocampus_common/ftf.py
+++ src/hippocampus_common/ftf.py
@@ -74,7 +74,7 @@ def tf_baselink_to_aircraft(orientation_q):
 
     Returns:
         pyquaternion.Quaternion: Orientation in aircraft frame.
-    """    
+    """
     return _transform_orientation("body_frames", orientation_q)
 
 
@@ -86,7 +86,7 @@ def tf_aircraft_to_baselink(orientation_q):
 
     Returns:
         pyquaternion.Quaternion: Orientation in baselink frame.
-    """    
+    """
     return _transform_orientation("body_frames", orientation_q)
 
 
@@ -99,7 +99,7 @@ def tf_ned_to_enu(orientation_q):
 
     Returns:
         pyquaternion.Quaternion: Orientation with ENU as reference frame.
-    """    
+    """
     return _transform_orientation("global_frames", orientation_q)
 
 
@@ -112,11 +112,11 @@ def tf_enu_to_ned(orientation_q):
 
     Returns:
         pyquaternion.Quaternion: Orientation with NED as reference frame.
-    """    
+    """
     return _transform_orientation("global_frames", orientation_q)
 
 
-def tf_gazebo_to_px4(orientation_q):
+def orientation_gazebo_to_px4(orientation_q):
     """ Transformation from gazebo frame to px4 frame.
     
     Twist of the baselink to the static ENU frame <-> Twist of the airframe to
@@ -130,8 +130,7 @@ def tf_gazebo_to_px4(orientation_q):
         pyquaternion.Quaternion: Orientation of the airframe in respect to the
             NED frame.
     """
-    return tf_enu_to_ned(
-        tf_baselink_to_aircraft(orientation_q))
+    return tf_enu_to_ned(tf_baselink_to_aircraft(orientation_q))
 
 
 def orientation_px4_to_gazebo(orientation_q):
@@ -148,8 +147,35 @@ def orientation_px4_to_gazebo(orientation_q):
         pyquaternion.Quaternion: Orientation of the baselink in respect to the
             ENU frame.
     """
-    return tf_aircraft_to_baselink(
-        tf_ned_to_enu(orientation_q))
+    return tf_aircraft_to_baselink(tf_ned_to_enu(orientation_q))
+
+
+def position_gazebo_to_px4(position_x, position_y, position_z):
+    """Transforms position from gazebo's ENU frame to px4's NED frame.
+
+    Args:
+        position_x (float): x-coordinate in ENU frame.
+        position_y (float): y-coordinate in ENU frame.
+        position_z (float): z-coordinate in ENU frame.
+
+    Returns:
+        tuple: Tuple of the transformed position.
+    """
+    return (position_y, position_x, -position_z)
+
+
+def position_px4_to_gazebo(position_x, position_y, position_z):
+    """Transforms position from px4's NED frame to gazebo's ENU frame.
+
+    Args:
+        position_x (float): x-coordinate in NED frame.
+        position_y (float): y-coordinate in NED frame.
+        position_z (float): z-coordinate in NED frame.
+
+    Returns:
+        tuple: Tuple of the transformed position.
+    """
+    return (position_y, position_x, -position_z)
 
 
 if __name__ == "__main__":
